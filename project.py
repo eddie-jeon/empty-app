fa85baeee498e1e5d780459e6e57eea2e2a6241c
eddie-jeon <zedwardjeone@gmail.com>
2016-07-21 15:17:10 -0400

    Updated from Brython Server: 7/21/2016 3:17:09 PM

--- project.py
+++ project.py
@@ -1,18 +1,69 @@
 from ggame import App, RectangleAsset, ImageAsset, SoundAsset, Sprite, Sound
+import random
 from ggame import LineStyle, Color
 
-# A ball! This is already in the ggame-tutorials repository
-ball_asset = ImageAsset("orb-150545_640.png")
-ball = Sprite(ball_asset, (0, 0))
-ball.fxcenter = 0.6
-ball.fyxenter = 0.6
-ball.scale = 0.05
+blocklist = []
 
-# Handle the mouse movement
-def mouseMove(event):
-    ball.x = event.x
-    ball.y = event.y
+black1= Color(0x000000, 1)
+black2 = Color(0x292929, 1)
+black2 = Color(0x404040, 1)
+black3 = Color(0x545454, 1)
+black4 = Color(0x6E6E6E, 1)
+black5 = Color(0x7A7A7A, 1)
+black6 = Color(0x8C8C8C, 1)
+black7 = Color(0xB0B0B0, 1)
+black8 = Color(0xCFCFCF, 1)
+black9 = Color(0xE3E3E3, 1)
+black10 = Color(0xF5F5F5, 1)
+black11 = Color(0xFCFCFC, 1)
+thinline1 = LineStyle(1, black1)
+thinline2 = LineStyle(1, black2)
+thinline3 = LineStyle(1, black3)
+thinline4 = LineStyle(1, black4)
+thinline5 = LineStyle(1, black5)
+thinline6 = LineStyle(1, black6)
+thinline7 = LineStyle(1, black7)
+thinline8 = LineStyle(1, black8)
+thinline9 = LineStyle(1, black9)
+thinline10 = LineStyle(1, black10)
+thinline11 = LineStyle(1, black11)
+rectangle1 = RectangleAsset(50, 50, thinline1, black1)
+rectangle2 = RectangleAsset(50, 50, thinline2, black2)
+rectangle3 = RectangleAsset(50, 50, thinline3, black3)
+rectangle4 = RectangleAsset(50, 50, thinline4, black4)
+rectangle5 = RectangleAsset(50, 50, thinline5, black5)
+rectangle6 = RectangleAsset(50, 50, thinline6, black6)
+rectangle7 = RectangleAsset(50, 50, thinline7, black7)
+rectangle8 = RectangleAsset(50, 50, thinline8, black8)
+rectangle9 = RectangleAsset(50, 50, thinline9, black9)
+rectangle10 = RectangleAsset(50, 50, thinline10, black10)
+rectangle11 = RectangleAsset(50, 50, thinline11, black11)
+
+
+
+    
+blocklist[0] = Sprite(rectangle1, (random.randint(0, 1678), random.randint(0, 772)))
+blocklist[1] = Sprite(rectangle2, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle3, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle4, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle5, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle6, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle7, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle8, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle9, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle10, (random.randint(0, 1678), random.randint(0, 772)))
+Sprite(rectangle11, (random.randint(0, 1678), random.randint(0, 772)))
+
+for s in blocklist:
+    s.visible = False
+    
+blocklist[0].visible = True
+
+blockcount = 0
+
+blocklist[blockcount].visible = False
+blockcount = blockcount + 1
+blocklist[blockcount].visible = True
 
 myapp = App()
-myapp.listenMouseEvent('mousemove', mouseMove)
 myapp.run()
